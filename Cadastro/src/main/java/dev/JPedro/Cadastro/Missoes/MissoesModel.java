2f5f1545588737cd7c8add4df0f4770a38448aaf
dev.JPedro <ps309205@gmail.com>
2025-06-10 13:26:30 -0300

    feat: Adiciona a dependencia Lombok na classe para abstração ao NinjaModel e MissoesModel

--- Cadastro/src/main/java/dev/JPedro/Cadastro/Missoes/MissoesModel.java
+++ Cadastro/src/main/java/dev/JPedro/Cadastro/Missoes/MissoesModel.java
@@ -1,10 +1,17 @@
 package dev.JPedro.Cadastro.Missoes;
 import dev.JPedro.Cadastro.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missoes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class MissoesModel {
 
     @Id
@@ -15,8 +22,5 @@ public class MissoesModel {
     // @OneToMany = uma missao pode ter varios ninjas
     @OneToMany(mappedBy = "missoes")
     private List<NinjaModel> ninjas;
-
-
-
 
 }
